6944ac5b44d272e095bd48fdf06c62a6e8eaced9
Bloom037 <bloom037@outlook.com>
2025-01-12 22:07:41 +0800

    update Header & Sidebar

---
+++ frontend/src/routes/layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { onMount, setContext } from "svelte";
+    import "../app.css";
+    import Header from "./Header.svelte";
+    import Sidebar from "./Sidebar.svelte";
+    import axios from "axios";
+    import getI18nStore from "../locales/index";
+    import { Button } from "flowbite-svelte";
+    const i18n = getI18nStore();
+  
+    const t = $i18n.t;
+    setContext("i18n", i18n);
+    setContext("t", t);
+
+</script>
+  
+<Header />
+<div class="w-full h-screen pt-2 flex flex-row">
+    <div>
+        <Sidebar />
+    </div>
+</div>
+  
