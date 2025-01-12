a3a6ab6e33a18d52ca74e82d208a1374ff6f0ac7
Bloom037 <bloom037@outlook.com>
2025-01-12 22:22:32 +0800

    update deduplication

--- frontend/src/routes/Sidebar.svelte
+++ frontend/src/routes/Sidebar.svelte
@@ -19,6 +19,7 @@
       t("sidebar.deployment_manager"),
       t("sidebar.error_log"),
       t("sidebar.settings"),
+      t("sidebar.deduplication")
     ];
     let route = [
       "/data",
@@ -27,6 +28,7 @@
       "/deployment",
       "/fault",
       "/config",
+      "/deduplication"
     ];
     $: activeUrl = (function() {
       // find the closest match of $page.url.pathname and route
@@ -38,6 +40,7 @@
     // 	'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5 fixed right-4 top-2 z-50';
   
     const icons = [
+      MailBoxOutline,
       MailBoxOutline,
       MailBoxOutline,
       MailBoxOutline,
