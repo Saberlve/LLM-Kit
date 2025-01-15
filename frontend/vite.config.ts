1488afdfcc88dffb7a449833b6dad35addaa6cb6
Braveliu66 <15185340517@163.com>
2025-01-16 00:13:04 +0800

    Add specific files from src/routes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,31 +1,6 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import compression from "vite-plugin-compression";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    compression({
-      algorithm: "gzip",
-      threshold: 10240,
-    }),
-  ],
-  build: {
-    minify: "terser", // enable minification
-    chunkSizeWarningLimit: 512, // set warning limit to 512kb
-  },
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://backend:8000/",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ""),
-//       },
-//       "/net": {
-//         target: "http://proxy:80/",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/net/, "/net"),
-//       },
-//     },
-//   },
+	plugins: [sveltekit()]
 });
