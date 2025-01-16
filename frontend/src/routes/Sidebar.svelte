d45418f98e0ea4ccba39a148adaf649914b569cd
Braveliu66 <15185340517@163.com>
2025-01-16 19:24:45 +0800

    Add specific files from src/routes

--- frontend/src/routes/Sidebar.svelte
+++ frontend/src/routes/Sidebar.svelte
@@ -13,20 +13,18 @@
   const t: any = getContext("t");
   // let sidebar = t("sidebar")
   let sidebar = [
-    t("sidebar.model_squre"),
-    t("sidebar.finetune_manager"),
     t("sidebar.data_manager"),
+    t("sidebar.dataset_construct"),
+    t("sidebar.quality_eval"),
     t("sidebar.deployment_manager"),
-    t("sidebar.model_download"),
     t("sidebar.error_log"),
     t("sidebar.settings"),
   ];
   let route = [
-    "/model",
-    "/finetune",
     "/data",
+    "/construct",
+    "/quality_eval",
     "/deployment",
-    "/download",
     "/fault",
     "/config",
   ];
@@ -45,13 +43,7 @@
     MailBoxOutline,
     MailBoxOutline,
     MailBoxOutline,
-    MailBoxOutline,
-    MailBoxOutline,
-    MailBoxOutline,
-    MailBoxOutline,
-    MailBoxOutline,
-    MailBoxOutline,
-    MailBoxOutline,
+    MailBoxOutline
   ];
 </script>
 
@@ -65,13 +57,13 @@
             <SidebarItem label={side} href={route[index]}>
               <svelte:fragment slot="icon">
                 <svelte:component
-                  this={icons[index]}
-                  class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                        this={icons[index]}
+                        class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                 />
               </svelte:fragment>
               <!-- <svelte:fragment slot="subtext">
-								<span class="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"> Pro </span>
-							</svelte:fragment> -->
+                                <span class="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"> Pro </span>
+                            </svelte:fragment> -->
             </SidebarItem>
           {/each}
         </SidebarGroup>
