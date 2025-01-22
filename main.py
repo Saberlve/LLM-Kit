80b21c1bf5d56af8afdd9f08aca645e98ea6225e
lizigao233 <507484868@qq.com>
2025-01-23 02:15:51 +0800

    parse与tex接口修复

--- main.py
+++ main.py
@@ -56,3 +56,69 @@ async def clear_data():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+"""
+import os
+
+from deduplication.qa_deduplication import QADeduplication
+from generate_qas.qa_generator import QAGenerator
+from quality_control.quality_control import QAQualityGenerator
+from text_parse.parse import parse
+from text_parse.to_tex import LatexConverter
+from utils.hparams import DedupParams, HyperParams
+
+def dedup():
+    hparams=DedupParams.from_dedup_yaml('hparams/dedup.yaml')
+    qa_dedup=QADeduplication(hparams)
+    qa_dedup.process_qa_file(hparams)
+
+def main():
+    hparams=HyperParams.from_hparams('hyparams/config.yaml')
+
+    file_list=[]  #待处理文件列表
+    if os.path.isdir(hparams.file_path):
+        files=os.listdir(hparams.file_path)
+        for file in files:
+            file_list.append(file)
+    elif os.path.isfile(hparams.file_path):
+        file_list.append(hparams.file_path)
+
+
+    for file in file_list:
+        print('Start iterative optimization of ' + os.path.basename(file))
+        parsed_file_path=parse(hparams)  #对原始材料进行解析，返回解析后文本数据
+        latex_converter=LatexConverter(parsed_file_path, hparams)
+        if file.split('.')[-1]!='tex' and hparams.convert_to_tex==True:
+            latex_converter.convert_to_latex()
+        elif file.split('.')[-1]=='tex':
+            pass
+            # 仍然
+        
+        qa_generator=QAGenerator(latex_converter.save_path, hparams)
+        qa_path=qa_generator.convert_tex_to_qas()
+
+        # 质量控制
+        quality_control=QAQualityGenerator(qa_path, hparams)
+        it_path=quality_control.iterate_optim_qa()
+        
+        
+
+            
+
+if __name__=='__main__':
+    main()
+
+        
+        
+        
+    
+
+            
+                
+    
+ 
+        
+    
+
+"""
